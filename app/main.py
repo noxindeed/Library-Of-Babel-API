bfe2329bc9ef287f854d207d199b756679e738d7
noxindeed <180201533+noxindeed@users.noreply.github.com>
2026-08-01 23:33:40 +0530

    experimenting main

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 import sys
 from pathlib import Path
-
 from fastapi import FastAPI
 
+app = FastAPI(title="Library of Babel API", description="an API for the Library of Babel written in python", version="0.1.0")
+
+from app.babel import (
+    get_page_by_address,
+    search_text_at_slot,
+) #noqa: E402
 ROOT = Path(__file__).resolve().parent.parent
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
-
-
-app = FastAPI()
 
 @app.get("/")
 def root():
